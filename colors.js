3288fd124c845d97e993eff20a7c43b3f6fdbff3
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2023-05-15 21:47:17 +0400

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,152 @@
+const colorPalettes = [
+    [
+        {
+            altStroke: '#1a2f38',
+            fill: '#264653'
+        },
+        {
+            altStroke: '#1b4d4c',
+            fill: '#287271'
+        },
+        {
+            altStroke: '#1b665d',
+            fill: '#2a9d8f'
+        },
+        {
+            altStroke: '#6b8a60',
+            fill: '#8ab17d'
+        },
+        {
+            altStroke: '#898a54',
+            fill: '#babb74'
+        },
+        {
+            altStroke: '#ba9d56',
+            fill: '#e9c46a'
+        },
+        {
+            altStroke: '#c2804c',
+            fill: '#f4a261'
+        },
+        {
+            altStroke: '#b5573f',
+            fill: '#e76f51'
+        },
+        {
+            altStroke: '#9e789e',
+            fill: '#ba8fba'
+        },
+        {
+            altStroke: '#42466e',
+            fill: '#595F94'
+        },
+        {
+            altStroke: '#661c0a',
+            fill: '#F3A8BE'
+        },
+        {
+            altStroke: '#912b11',
+            fill: '#b43718'
+        }
+    ],
+    [
+        {
+            altStroke: '#2A415B',
+            fill: '#355070'
+        },
+        {
+            altStroke: '#3A3D53',
+            fill: '#515575'
+        },
+        {
+            altStroke: '',
+            fill: '#6d597a'
+        },
+        {
+            altStroke: '#4B3D54',
+            fill: '#915f78'
+        },
+        {
+            altStroke: '#965361',
+            fill: '#b56576'
+        },
+        {
+            altStroke: '',
+            fill: '#b56576'
+        },
+        {
+            altStroke: '#965462',
+            fill: '#cd6873'
+        },
+        {
+            altStroke: '#C1595D',
+            fill: '#e56b6f'
+        },
+        {
+            altStroke: '#BD7166',
+            fill: '#e88c7d'
+        },
+        {
+            altStroke: '',
+            fill: '#eaac8b'
+        },
+        {
+            altStroke: '#C48E71',
+            fill: '#FFC5BC'
+        },
+        {
+            altStroke: '#BF6A6D',
+            fill: '#E48184'
+        }
+    ],
+    [
+        {
+            altStroke: '#24052F',
+            fill: '#540d6e'
+        },
+        {
+            altStroke: '#621740',
+            fill: '#a1286a'
+        },
+        {
+            altStroke: '#C23453',
+            fill: '#ee4266'
+        },
+        {
+            altStroke: '#CB6837',
+            fill: '#f78a53'
+        },
+        {
+            altStroke: '#CC6938',
+            fill: '#fbae49'
+        },
+        {
+            altStroke: '#DAAF22',
+            fill: '#ffd23f'
+        },
+        {
+            altStroke: '#87B85E',
+            fill: '#9dd076'
+        },
+        {
+            altStroke: '#2FA78B',
+            fill: '#3bceac'
+        },
+        {
+            altStroke: '#1A8C66',
+            fill: '#25be8b'
+        },
+        {
+            altStroke: '#24052F',
+            fill: '#540d6e'
+        },
+        {
+            altStroke: '#09854F',
+            fill: '#0ead69'
+        },
+        {
+            altStroke: '#621740',
+            fill: '#a1286a'
+        }
+    ]
+]
